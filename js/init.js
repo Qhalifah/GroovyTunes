0c7cc40bfe8dac144a1960feb06f428ef18b5cb5
Tyler Huning <thuning@hotmail.com>
2017-09-20 16:18:22 -0500

    prettified files

--- js/init.js
+++ js/init.js
@@ -1,33 +1,33 @@
 var socket = null;
 var isopen = false;
 window.onload = function() {
-            socket = new WebSocket("ws://127.0.0.1:9000");
-            socket.onopen = function() {
-               console.log("Connected!");
-               isopen = true;
-               socket.send('getMusic')
-            }
-            socket.onmessage = function(e) {
-              var json = JSON.parse(e.data);
-              console.log(json)
-              if(json.songs){
-                //dispSongs(json.songs)
-              }
-            }
+  socket = new WebSocket("ws://127.0.0.1:9000");
+  socket.onopen = function() {
+    console.log("Connected!");
+    isopen = true;
+    socket.send('getMusic')
+  }
+  socket.onmessage = function(e) {
+    var json = JSON.parse(e.data);
+    console.log(json)
+    if (json.songs) {
+      //dispSongs(json.songs)
+    }
+  }
 
-            socket.onclose = function(e) {
-               console.log("Connection closed.");
-               socket = null;
-               isopen = false;
-            }
-         };
+  socket.onclose = function(e) {
+    console.log("Connection closed.");
+    socket = null;
+    isopen = false;
+  }
+};
 
 
 
-function dispSongs(_songs){
+function dispSongs(_songs) {
   var html = ''
-  _.each(_songs, function(_song){
-    html+='<div><audio controls><source src="../songs/' + _song + '" type="audio/mpeg">Your browser does not support the audio element.</audio></div>'
+  _.each(_songs, function(_song) {
+    html += '<div><audio controls><source src="../songs/' + _song + '" type="audio/mpeg">Your browser does not support the audio element.</audio></div>'
   });
   console.log(html);
   $('#songs').html(html);

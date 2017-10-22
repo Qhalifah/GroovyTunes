d2629f7f5bba9df4823bdf5e79a1891bc417b950
Rupesh <rpancholi15@gmail.com>
2017-10-22 15:34:01 -0500

    Adds OpenCSV Jar and Updates Playlist.java
    Adds funtionality for adding and removing playlists from csv.

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,9 +1,11 @@
 import java.util.*;
 import java.util.stream.Collectors;
 import java.io.*;
+import com.opencsv.*;
 
 public class Playlist{
 
+	private static final String PLAYLISTS_DATABASE = "./databases/playlists.csv"; // Location of playlists.csv file
 	private String playlistName;
 	public String playlistId;
 	public Date createDate;
@@ -42,23 +44,73 @@ public class Playlist{
 		return "";
 	}
 	
-	public static void updatePlaylist(String playlistId, ArrayList<String> songs) throws IOException{
-		FileWriter writer = new FileWriter("./databases/playlists.csv");
-		String songList = arrayListParser(songs);
-		writer.write("\"playlistId\"," + "\"" + songList + "\"" + "\n")
-		writer.flush();
-		writer.close();
+	public static void updatePlaylist(String playlistId, ArrayList<String> songs){
+			// Removed existing playlist from database
+		removePlaylist(playlistId);
+		
+		try{
+			CSVWriter writer = new CSVWriter(new FileWriter(PLAYLISTS_DATABASE, true)); // true flag for appending to end of file
+			String songList = arrayListParser(songs);
+			String[] record = (playlistId + "," + songList).split(",");
+			writer.writeNext(record);
+			//writer.write("\"" + playlistId + "\"" + "," + "\"" + songList + "\"" + "\n")
+			writer.flush();
+			writer.close();
+		}
+		
+		catch(Exception e){
+			e.printStackTrace();
+		}
 		
 	}
 	
-	public void updatePlaylist(String playlistId){
+		// Reads in all playlists from playlists.csv
+		// Removes the playlist based on playlistId
+		// Rewrites playlists back into playlists.csv
+	public static void removePlaylist(String playlistId){
+		try{
+				//Build reader instance
+				//Read playlists.csv
+				//Default seperator is comma
+				//Default quote character is double quote
+				//Start reading from line number 1 (line numbers start from zero)
+			CSVReader reader = new CSVReader(new FileReader(PLAYLISTS_DATABASE), ',', '"', 0);
+			List<String[]> allRows = reader.readAll();
+
+				// Iterator created for List of records
+				// For each String[] record, if 0th element (playlistId) is the one to be deleted, it is removed from List
+			for (Iterator<String[]> iterator = allRows.listIterator(); iterator.hasNext(); ){
+				String[] record = iterator.next();
+				if (record[0].equals(playlistId)) {
+					iterator.remove();
+				}
+			}
+			
+			/* 			int index = -1; 
+			for(int i = 0; i < allRows.length; i++){
+				if(allRows.get(i)[0].equals(playlistId)){
+					index = i;
+					break;
+				}
+			}
+			if(index >= 0){
+				allRows.remove(index);
+			} 
+			*/
 		
-	}
-	
-	public static void removePlaylist(){
+				// Writes List of playlists back into playlists.csv
+			CSVWriter writer = new CSVWriter(new FileWriter(PLAYLISTS_DATABASE));
+			writer.writeAll(allRows);
+			writer.flush();
+            writer.close();
+		}
 		
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
-	
+		// Converts ArrayList of Strings into a single comma seperated string
+		// of all songs in playlist
 	private static String arrayListParser(ArrayList<String> songs){
 		String collect = songs.stream().collect(Collectors.joining(","));
 		return collect;
